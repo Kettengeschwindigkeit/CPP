a6b45a9cec98dd90309a991c192fa77d2a66ccc3
Kettengeschwindigkeit <666damned999@mail.ru>
2023-10-14 21:01:29 +0300

    #004 escape sequences (homework)

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 using namespace std;
 
-/*
-	\n - перейти на начало новой строки
-	\t - перейти к следующей позиции табул€ции
-	\b - удаление последнего выведенного символа
-
-	\\ - вывести обратную косую черту
-	\" - вывести двойную кавычку
-	\' - вывести одинарную кавычку
-*/
-
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-
-	cout << "Ћенин\n";
-	cout << "\t\tЋенин\n\n\t\n";
-	cout << "\"Ћенин\"\\\n";
+	cout << "I gotta flag and a picture of Robert E. Lee\nHangin' in the den for everyone to see\nI got a 68 long bed \"GMC\"\nSittin' out back underneath a tree\n";
+	cout << "Every-day I'm tired cause I work really hard\nAt 5 o'clock I punch my time card\nSo when I get home I don't pay no re-gard\nTo the junk that's pilin' up in my back-yard\n\n";
+	cout << "\tWell you can wash The \"Dukes of Hazzard\" sheets on my bed\n\tYou can wash the dust off of my stuffed deer head\n\tYou can wash my mud tires 'til there's no tread\n\tBut you can't wash the red outta my redneck\n";
 }
