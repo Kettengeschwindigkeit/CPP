19a828af502d42531294232f0d22cba43c6510ab
Kettengeschwindigkeit <666damned999@mail.ru>
2023-12-28 21:04:27 +0300

    #007 constants

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,41 +1,14 @@
 #include <iostream>
 using namespace std;
 
-/*
-	Переменная - это область оперативной памяти, обладающая собственным именем и предназначенная для хранения данных разных типов, которые могут быть изменены
-*/
-
 void main()
 {
-	setlocale(LC_ALL, "Rus");
+    setlocale(LC_ALL, "Rus");
 
-	int a;
-	a = 5;
+    const int COUNT_DAYS_IN_WEEK = 7;
 
-	int b = 7;
+    const char NEW_LINE = '\n';
+    const char TAB = '\t';
 
-	cout << a << endl;
-	cout << b << endl;
-
-	a = 10;
-
-	cout << a << endl;
-
-	int Age, c, d, e = 6565;
-
-	Age = 10;
-	c = 1;
-	d = 2;
-
-	cout << Age << " " << c << " " << d << " " << e << endl;
-
-	double f = 1.1551;
-	char g = 'g';
-
-	cout << f << " " << g << endl;
-
-	bool h = true;
-	bool i = false;
-
-	cout << h << " " << i << endl;
+    cout << TAB << TAB << COUNT_DAYS_IN_WEEK << NEW_LINE;
 }
